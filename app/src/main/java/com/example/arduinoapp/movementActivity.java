9de8c270cd91d45a1787ea6987d0327c59a1d3fb
danielS2001 <saenzdaniel2001@outlook.com>
2022-08-11 19:47:06 -0600

    Primer Commit
    En este commit se agregan los principales archivos de la aplicación

---
+++ app/src/main/java/com/example/arduinoapp/movementActivity.java
@@ -0,0 +1,43 @@
+package com.example.arduinoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class movementActivity extends AppCompatActivity {
+    private Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movement);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder confirmar = new AlertDialog.Builder(movementActivity.this);
+                confirmar.setTitle("¿Cerrar?");
+                confirmar.setMessage("¿Está seguro de que desea regresar al menu?");
+                confirmar.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                confirmar.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                confirmar.show();
+            }
+        });
+
+    }
+}
